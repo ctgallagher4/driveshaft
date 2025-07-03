8a59f909fc56eade86bffc6173db374a150bf87f
Charlie Gallagher <ctgall4@gmail.com>
2025-07-03 17:09:43 -0400

    implement adaptive backoff

--- src/actor.rs
+++ src/actor.rs
@@ -18,16 +18,34 @@ impl Actor {
     ) -> Self {
         let handle = std::thread::spawn(move || {
             core_affinity::set_for_current(core_id);
+            let mut spin_count = 0;
+
             loop {
                 let job = fifo.pop().or_else(|| {
-                    iter::repeat_with(|| {
+                    let steal = iter::repeat_with(|| {
                         global
                             .steal_batch_and_pop(&fifo)
                             .or_else(|| stealers.iter().map(|s| s.steal()).collect())
                     })
                     .find(|s| !s.is_retry())
-                    .and_then(|s| s.success())
+                    .and_then(|s| s.success());
+
+                    if steal.is_none() {
+                        spin_count += 1;
+                        if spin_count < 10 {
+                            std::hint::spin_loop();
+                        } else if spin_count < 100 {
+                            std::thread::yield_now();
+                        } else {
+                            std::thread::sleep(std::time::Duration::from_micros(50));
+                        }
+                    } else {
+                        spin_count = 0; // reset on success
+                    }
+
+                    steal
                 });
+
                 if let Some(job) = job {
                     let _ = job(&mut ctx);
                 }
